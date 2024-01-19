99d00af5d448eff04870340f21307903ccebbd53
DaveC <dcarioto114@outlook.com>
2024-01-19 14:17:36 -0500

    add all hour blocks, click event to save, loop to display loacl data and color coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,63 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+var thisDay = dayjs();
+var displayDate = document.querySelector("#currentDay");
+var btnClick = $('.saveBtn');
+var localDesc = document.querySelector(".description");    
+
+
+$(document).ready(function () {
+  displayDate.textContent = thisDay.format('MMMM DD, YYYY');
+  var nowHour = thisDay.format('[hour-]HH');
+  
+  $(".time-block").each(function() {
+    var hourBlock = $(this).attr("id")
+    var commentBlock = localStorage.getItem($(this).attr("id"));
+    if (nowHour > hourBlock){
+      
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+      $(this).children(".description").val(commentBlock);
+
+    }
+    else if (nowHour === hourBlock){
+      $(this).removeClass("future");
+      $(this).removeClass("past");
+      $(this).addClass("present");
+      $(this).children(".description").val(commentBlock);
+
+    }
+    else {
+    $(this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).addClass("future");
+    $(this).children(".description").val(commentBlock);
+    }});
+
+  
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  btnClick.on("click", function(){
+
+   var appTime = $(this).parent().attr("id");
+   var apptText = $(this).siblings(".description").val();
+    console.log(appTime);
+    console.log(apptText);
+   localStorage.setItem(appTime,apptText);
+   
+  })
+   
+
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -20,4 +70,4 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+ });
